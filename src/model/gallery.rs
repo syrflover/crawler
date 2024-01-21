ddc6ecfcfd7976c9824072886eadad95e999d8a6
syr <user@syrflover.co>
2024-01-21 22:57:09 +0900

    parsing date

--- src/model/gallery.rs
+++ src/model/gallery.rs
@@ -1,3 +1,4 @@
+use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 use super::*;
@@ -11,5 +12,5 @@ pub struct Gallery {
     pub files: Vec<(usize, File)>,
     pub language: Option<String>,
     pub tags: Vec<Tag>,
-    pub date: String,
+    pub date: DateTime<Utc>,
 }
