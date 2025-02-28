ed0b6d5122c804dcf3f6b787bc692d572cbd7797
syr <user@syrflover.co>
2025-02-28 17:58:18 +0900

    chore: rust 2024 edition

--- src/lib.rs
+++ src/lib.rs
@@ -13,11 +13,6 @@ pub type Result<T> = std::result::Result<T, Error>;
 #[cfg(test)]
 mod tests {
     pub fn tracing() {
-        let home_dir = std::env::var("HOME").unwrap();
-        let deno_install = format!("{home_dir}/.deno");
-        let path = std::env::var("PATH").unwrap();
-        std::env::set_var("PATH", format!("{deno_install}/bin:{path}"));
-
         if std::env::args().any(|arg| arg == "--nocapture") {
             let subscriber = tracing_subscriber::fmt()
                 .with_max_level(tracing::Level::DEBUG)
