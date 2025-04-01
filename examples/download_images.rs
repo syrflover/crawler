a645ce0da5fa2822aee50dbe34d1a91db833a32d
syr <user@syrflover.co>
2025-04-01 15:39:06 +0900

    update examples/download_images.rs

--- examples/download_images.rs
+++ examples/download_images.rs
@@ -1,11 +1,10 @@
 use std::path::PathBuf;
 
-use futures::{StreamExt, TryStreamExt, stream};
+use futures::{stream, StreamExt, TryStreamExt};
 use hitomi_la::{
     gallery,
     gg::GG,
     image::{self, Image, ImageExt, ImageKind},
-    model::Gallery,
     nozomi::{self, Language},
 };
 use tokio::fs;
@@ -14,53 +13,58 @@ use tokio::fs;
 async fn main() -> anyhow::Result<()> {
     let base_dir = PathBuf::from("./galleries");
 
-    let ids = nozomi::parse(Language::Korean, 1, 3).await?;
+    let ids = nozomi::parse(Language::Korean, 1, 24).await?;
 
     println!("nozomi: {:?}", ids);
 
     let gg = GG::from_hitomi().await?;
 
-    for id in ids {
-        let Some(gallery): Option<Gallery> = gallery::parse(id).await? else {
-            eprintln!("not exists gallery {} in hitomi", id);
-            continue;
-        };
+    let galleries = stream::iter(ids)
+        .map(gallery::parse)
+        .buffered(8)
+        .try_collect::<Vec<_>>()
+        .await?;
 
-        println!("gallery: {:?}", gallery);
+    let gallery = galleries
+        .into_iter()
+        .flatten()
+        .min_by_key(|g| g.files.len())
+        .unwrap();
 
-        let gallery_dir = base_dir.join(id.to_string());
+    println!("gallery: {:?}", gallery);
 
-        println!("create dir: {}", gallery_dir.as_os_str().to_string_lossy());
+    let gallery_dir = base_dir.join(gallery.id.to_string());
 
-        fs::create_dir_all(&gallery_dir).await?;
+    println!("create dir: {}", gallery_dir.as_os_str().to_string_lossy());
 
-        stream::iter(gallery.files)
-            // page: starts from 1
-            .map(|(page, file)| {
-                let gg = &gg;
-                let gallery_dir = &gallery_dir;
+    fs::create_dir_all(&gallery_dir).await?;
 
-                async move {
-                    println!("download: start {}", page);
+    stream::iter(gallery.files)
+        // page: starts from 1
+        .map(|(page, file)| {
+            let gg = &gg;
+            let gallery_dir = &gallery_dir;
 
-                    let image: Image =
-                        image::download(&file, ImageKind::Original, ImageExt::Avif, gg).await?;
+            async move {
+                println!("download: start {}", page);
 
-                    println!("download: complete {}", page);
+                let image: Image =
+                    image::download(&file, ImageKind::Original, ImageExt::Avif, gg).await?;
 
-                    let image_dir = gallery_dir.join(format!("{}.{}", page, image.ext));
+                println!("download: complete {}", page);
 
-                    fs::write(&image_dir, image.buf).await?;
+                let image_dir = gallery_dir.join(format!("{}.{}", page, image.ext));
 
-                    println!("write: {}", image_dir.as_os_str().to_string_lossy());
+                fs::write(&image_dir, image.buf).await?;
 
-                    Ok::<(), anyhow::Error>(())
-                }
-            })
-            .buffer_unordered(4)
-            .try_collect::<()>()
-            .await?;
-    }
+                println!("write: {}", image_dir.as_os_str().to_string_lossy());
+
+                Ok::<(), anyhow::Error>(())
+            }
+        })
+        .buffer_unordered(4)
+        .try_collect::<()>()
+        .await?;
 
     Ok(())
 }
